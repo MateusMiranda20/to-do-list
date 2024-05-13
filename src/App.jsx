0a1f4787087e00f0c2fd9ce5efb317234a2ed7d0
MateusMiranda20 <Mateuusmiranda20@gmail.com>
2024-05-13 13:49:57 -0300

    Finished the application

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@
 import React, { useState } from 'react';
 import { v4 as uuid } from 'uuid';
 
-import { Container, ToDoList, Input, Button, ListItem} from './styles.js'
-
-import { FcCheckmark, FcEmptyTrash  } from "react-icons/fc";
+import { Container, ToDoList, Input, Button, ListItem, Check, Trash } from './styles.js'
 
 function App() {
 
-  const [list, setList] = useState([{ id: uuid(), task: 'Trabalhar amanhã' }])
+  const [list, setList] = useState([])
   const [inputTask, setInputTask] = useState('')
 
 
@@ -18,28 +16,48 @@ function App() {
   }
 
   function cliqueiNoBotao() {
-    setList([...list, { id: uuid(), task: inputTask }])
+    if(inputTask) {
+      setList([...list, { id: uuid(), task: inputTask, finished: false }])
+    }
+  }
+
+  function finishTask(id) {
+    const newList = list.map(item => (
+      item.id === id ? { ...item, finished: !item.finished } : item
+    ))
+
+    setList(newList)
+  }
+
+  function deleteTask(id) {
+    const deleteList = list.filter(item => item.id !== id)
+
+    setList(deleteList)
   }
 
   return (
     <>
       <Container>
-       <ToDoList>
-       <Input onChange={inputMudou} placeholder="O que eu devo fazer?.." />
-        <Button onClick={cliqueiNoBotao}>Adicionar</Button>
+        <ToDoList>
+          <Input onChange={inputMudou} placeholder="O que eu devo fazer?.." />
+          <Button onClick={cliqueiNoBotao}>Adicionar</Button>
 
-        <ul>
-          {
-            list.map(item => (
-             <ListItem>
-                <FcCheckmark />
-                <li key={item.id}> {item.task}</li>
-                <FcEmptyTrash />
-             </ListItem>
-            ))
-          }
-        </ul>
-       </ToDoList>
+          <ul>
+            {
+              list.length > 0 ? (
+                  list.map(item => (
+                    <ListItem isFinished={item.finished} key={item.id}>
+                      <Check onClick={() => finishTask(item.id)} />
+                      <li>{item.task}</li>
+                      <Trash onClick={() => deleteTask(item.id)} />
+                    </ListItem>
+                  ))
+              ) : (
+              <h4>Não há tarefas</h4>
+              )
+            }
+          </ul>
+        </ToDoList>
       </Container>
 
     </>
--- src/styles.js
+++ src/styles.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+import { FcCheckmark, FcEmptyTrash } from "react-icons/fc";
+
 export const Container = styled.div`
     background: linear-gradient(90deg, #383838 0%, #000000 81.25%);
     width: 100vw;
@@ -12,17 +14,58 @@ export const ToDoList = styled.div`
   background: white;
   padding: 20px 30px;
   border-radius: 5px;
+
+  ul{
+    padding: 0;
+    margin-top: 50px;
+  }
 `
 export const Input = styled.input`
   border: 2px solid rgba(208, 211, 212, 0.4);
   border-radius: 5px ;
-  height: 25px;
-  width: 250px;
-  margin-right: 16px;
+  height: 40px;
+  width: 340px;
+  margin-right: 35px;
 `
-export const listItem = styled.div`
-    background: #e4e4e4;
-    box-shadow: 1px 4px 10px rgba(0, 0, 0, 0.2);
-    border-radius: 5px;
-    height: 40px;
+export const Button = styled.button`
+  background: #2ec;
+  border-radius: 5px;
+  font-weight: 900;
+  font-size: 15px;
+  line-height: 2px;
+  height: 40px;
+  border: none;
+  color: black;
+  cursor: pointer;
+
+  &:hover{
+    opacity: 0.8;
+  }
+
+  &:active{
+    opacity: 0.6;
+  }
+  
+`
+export const ListItem = styled.div`
+  background: ${props => props.isFinished ? '#e87' : '#e4e4e4'};
+  box-shadow: 1px 4px 10px rgba(0, 0, 0, 0.2);
+  border-radius: 5px;
+  height: 55px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 25px;
+  padding: 0 12px;
+  width: 500px;
+
+  li{
+    list-style: none;
+  }
+`
+export const Check = styled(FcCheckmark) `
+  cursor: pointer;
+`
+export const Trash = styled(FcEmptyTrash) `
+  cursor: pointer;
 `
